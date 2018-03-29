d3294323efd6543ecf230368a31e2b9c011fe74f
Nathaniel Woodthorpe <d12@github.com>
2018-03-28 20:59:46 -0400

    1kb img instead of 1mb img

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@ echo "Compiling to binary..."
 nasm -f bin -o snake.bin snake.asm
 
 echo "Copying compiled binary to img file"
-dd if=/dev/zero of=snake.img ibs=1k count=1440
+dd if=/dev/zero of=snake.img ibs=1k count=1
 dd if=snake.bin of=snake.img conv=notrunc
 
 echo "Tidying up..."
